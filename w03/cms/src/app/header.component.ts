fa998e5eabb0c95a9f5ea13adf4bc088d05c44fa
ItsMen <mvdo1091@gmail.com>
2025-05-13 00:51:21 -0400

    w03 initial commit

---
+++ w03/cms/src/app/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  standalone: false,
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
